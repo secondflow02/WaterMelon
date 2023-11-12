34d739e885f02f6a3f1c7eb5461daf2e984acf9c
hayoung78 <wendy4332@naver.com>
2023-11-13 00:41:50 +0900

    feat(hayoung)1113:watermelon page components생성

---
+++ src/pages/main/components/MelonCharts/Charts.js
@@ -0,0 +1,16 @@
+//차트 뿌려주는곳
+// 2페어 차필우 허진욱
+// const Charts = () => {
+//     return (
+//         <ul>
+//          <li> top100  </li>
+//          <li> pop     </li>
+//          <li> 아티스트 </li>
+//         </ul>
+//         <div>
+//             <ul>
+//                 charItem.map
+//             </ul>
+//         </div>
+//     )
+// }
