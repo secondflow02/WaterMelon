368f939aee99cf4b40a972c2f41f56a6dcefe2c8
KR-HeoJU <jwhur2863@gmail.com>
2023-11-15 21:27:58 +0900

    feat(KR-HeoJU)11/15 : Popularcontents&MelonCharts 생성

--- src/pages/main/mainPage.js
+++ src/pages/main/mainPage.js
@@ -1,1 +1,14 @@
 // 공용 2조
+
+import Charts from "./components/MelonCharts/Charts";
+import PopularContentsContainer from "./components/PopularContents/PopularContentsContainer";
+
+const MainPage = () => {
+    return (
+        <>
+            <PopularContentsContainer />
+            <Charts />
+        </>
+    );
+};
+export default MainPage;
