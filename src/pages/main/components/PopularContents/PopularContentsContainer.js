922f09cf736c9fac44251fd38644c27c970a9ee2
KR-HeoJU <jwhur2863@gmail.com>
2023-11-21 04:30:56 +0900

    feat(KR-HeoJU) 11/21 : PopularContents ellipsis 적용

--- src/pages/main/components/PopularContents/PopularContentsContainer.js
+++ src/pages/main/components/PopularContents/PopularContentsContainer.js
@@ -2,7 +2,7 @@
 // 2페어 차필우 허진욱
 import styled from "styled-components";
 import contentsData from "../../../../mock/contentsData.json";
-import { flexCenter } from "../../../../styles/common";
+import { flexAlignCenter, flexCenter } from "../../../../styles/common";
 
 const PopularContentsContainer = () => {
     return (
@@ -16,21 +16,25 @@ const PopularContentsContainer = () => {
                 </H2>
                 <SUB_LIST>
                     <ISSUE_LIST>
-                        {contentsData.contentsdata.map((el, index) => (
-                            <DL>
-                                <MOV_AREA>
-                                    <a href=";">
-                                        <IMG src={el.image} alt="" />
-                                        <BG_ALBUM_FRAME></BG_ALBUM_FRAME>
-                                    </a>
-                                </MOV_AREA>
-                                <DT>
-                                    <TITLE>{el.title}</TITLE>
-                                    <ELIPSIS>{el.singer}</ELIPSIS>
-                                </DT>
-                                <VIEW_NO>{el.category}</VIEW_NO>
-                            </DL>
-                        ))}
+                        {contentsData.contentsdata
+                            .slice(6, 12)
+                            .map((el, index) => (
+                                <DL>
+                                    <MOV_AREA>
+                                        <a href=";">
+                                            <IMG src={el.image} alt="" />
+                                            <BG_ALBUM_FRAME></BG_ALBUM_FRAME>
+                                        </a>
+                                    </MOV_AREA>
+                                    <DT>
+                                        <TITLE>{el.title}</TITLE>
+                                        <ELIPSIS>{el.singer}</ELIPSIS>
+                                    </DT>
+                                    <VIEW_NO>
+                                        <span>{el.category}</span>
+                                    </VIEW_NO>
+                                </DL>
+                            ))}
                     </ISSUE_LIST>
                 </SUB_LIST>
                 <WRAP_PAGE>
@@ -47,19 +51,14 @@ export default PopularContentsContainer;
 
 const HotIssueConTainer = styled.div`
     color: #666;
-    font: inherit;
-    vertical-align: baseline;
-    clear: both;
     width: 704px;
     position: relative;
-    float: left;
     height: 472px;
     margin-top: 32px;
     margin-bottom: 49px;
 `;
 
 const H2 = styled.h2`
-    font: inherit;
     height: 24px;
     color: #1a1a1a;
     font-size: 15px;
@@ -72,23 +71,11 @@ const H2 = styled.h2`
 `;
 
 const TITLE_LINK = styled.span`
-    display: block;
-    float: left;
     width: 70px;
     height: 15px;
-    background: url(//cdnimg.melon.co.kr/resource/image/web/main/bg_main_title_140716.png)
-        no-repeat;
-    background-position: 0 -194px;
-    text-indent: -9999px;
 `;
 
 const TXT_INTRO = styled.span`
-    font: inherit;
-    margin: 0;
-    padding: 0;
-    border: 0;
-    display: inline-block;
-    width: 300px;
     margin-top: 1px;
     padding-left: 12px;
     letter-spacing: 0;
@@ -99,102 +86,50 @@ const TXT_INTRO = styled.span`
     background: 0;
     vertical-align: top;
 `;
-const WRAP_PAGE = styled.div`
-    position: absolute;
-    top: 0;
-    right: 0;
-`;
-const PAGE = styled.div``;
-const PAGE_NUM = styled.span`
-    display: inline-block;
-    color: #888;
-    font-size: 12px;
-    line-height: 17px;
-    vertical-align: top;
-    font-family: arial, "돋움", "dotum", sans-serif, "Apple Gothic", sans-serif;
-`;
-
-const WRAP_BTN = styled.span`
-    vertical-align: top;
-    display: inline-block;
-    margin-left: 3px;
-`;
 
 const SUB_LIST = styled.ul`
-    letter-spacing: 0;
     color: #666;
-    -webkit-text-size-adjust: none;
     margin: 0;
     padding: 0;
     border: 0;
-    font: inherit;
-    list-style: none;
     font-weight: normal;
-    display: flex;
-    align-items: center;
-    justify-content: center;
+    ${flexAlignCenter}
 `;
 
 const ISSUE_LIST = styled.li`
-    letter-spacing: 0;
-    color: #666;
-    -webkit-text-size-adjust: none;
     margin: 0;
     padding: 0;
     border: 0;
-    font: inherit;
-    vertical-align: baseline;
-    list-style: none;
-    font-weight: normal;
-    clear: both;
     width: 704px;
-    overflow: hidden;
-    word-break: break-all;
-    text-overflow: ellipsis;
-    white-space: nowrap;
+    display: block;
 `;
 
 const DL = styled.dl`
-    letter-spacing: 0;
     color: #666;
-    list-style: none;
     padding: 0;
     border: 0;
-    font: inherit;
-    vertical-align: baseline;
-    margin-left: 0 !important;
     position: relative;
-    margin: 16px 10px 2px 0px;
+    margin: 16px 0px 2px 16px;
     float: left;
+    &:nth-of-type(1) {
+        margin-left: 0px;
+    }
+    &:nth-of-type(4) {
+        margin-left: 0px;
+    }
 `;
 
 const MOV_AREA = styled.dd`
-    letter-spacing: 0;
     color: #666;
-    -webkit-text-size-adjust: none;
-    list-style: none;
     margin: 0;
     padding: 0;
     border: 0;
-    font: inherit;
-    vertical-align: baseline;
-    overflow: hidden;
     width: 223px;
     height: 148px;
     position: relative;
 `;
 
 const IMG = styled.img`
-    letter-spacing: 0;
-    -webkit-text-size-adjust: none;
-    list-style: none;
-    color: #1a1a1a;
-    margin: 0;
-    padding: 0;
-    font: inherit;
-    vertical-align: baseline;
-    line-height: 0;
-    font-size: 0;
     border: 0;
     height: 100%;
     width: 100%;
@@ -202,15 +137,10 @@ const IMG = styled.img`
 `;
 
 const BG_ALBUM_FRAME = styled.a`
-    letter-spacing: 0;
-    list-style: none;
     color: #1a1a1a;
     margin: 0;
     padding: 0;
     border: 0;
-    font: inherit;
-    vertical-align: baseline;
-    position: absolute;
     top: 0;
     left: 0;
     width: 223px;
@@ -218,43 +148,36 @@ const BG_ALBUM_FRAME = styled.a`
 `;
 
 const DT = styled.dt`
-    letter-spacing: 0;
     color: #666;
-    -webkit-text-size-adjust: none;
-    list-style: none;
     margin: 0;
     padding: 0;
     border: 0;
-    font: inherit;
-    vertical-align: baseline;
     width: 223px;
     line-height: 16px;
+    font-size: 12px;
 `;
 
 const TITLE = styled.span`
+    display: block;
     letter-spacing: 0;
-    list-style: none;
     cursor: pointer;
     margin: 0;
     padding: 0;
     border: 0;
-    font: inherit;
-    vertical-align: baseline;
     height: 16px;
+    font: inherit;
     font-weight: bold;
     color: #333;
     margin-top: 6px;
     margin-bottom: 2px;
     overflow: hidden;
     word-break: break-all;
-    text-overflow: ellipsis;
     white-space: nowrap;
+    text-overflow: ellipsis;
 `;
 
 const ELIPSIS = styled.span`
     letter-spacing: 0;
-    -webkit-text-size-adjust: none;
-    list-style: none;
     color: #888;
     cursor: pointer;
     margin: 0;
@@ -262,12 +185,12 @@ const ELIPSIS = styled.span`
     border: 0;
     font: inherit;
     display: block;
-    width: 223px;
+    width: 200px;
     height: 16px;
     overflow: hidden;
     word-break: break-all;
-    text-overflow: ellipsis;
     white-space: nowrap;
+    text-overflow: ellipsis;
 `;
 
 const VIEW_NO = styled.dd`
@@ -286,4 +209,23 @@ const VIEW_NO = styled.dd`
     font-family: "Dotum", "Apple Gothic", sans-serif;
     color: green;
     height: 14px;
+`;
+
+const WRAP_PAGE = styled.div`
+    position: absolute;
+    top: 0;
+    right: 0;
+`;
+const PAGE = styled.div``;
+const PAGE_NUM = styled.span`
+    color: #888;
+    font-size: 12px;
+    line-height: 17px;
+    vertical-align: top;
+    font-family: arial, "돋움", "dotum", sans-serif, "Apple Gothic", sans-serif;
+`;
+
+const WRAP_BTN = styled.span`
+    vertical-align: top;
+    margin-left: 3px;
 `;
