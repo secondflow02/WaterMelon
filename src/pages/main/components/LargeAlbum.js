791cbfe5ec5407ad9c38b26e464760dac97cb5d7
keeprok <dldudfhr425@gmail.com>
2023-11-21 21:14:07 +0900

    feat(keeprok) 11/21 : src-components-LargeAlbum,main-comonents-LargeAlbum 사진 컴포넌트 및 index버튼 state값추가전

--- src/pages/main/components/LargeAlbum.js
+++ src/pages/main/components/LargeAlbum.js
@@ -3,11 +3,13 @@
 import { useEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
 
-import LargeData from '../../../mock/LargeAlbumData.json';
+import LargeData from '../../../components/LargeAlbum';
+//image어떻게 찍히는 지 확인
+import Image1 from '../../../components/LargeAlbum';
+// import img1 from 'https://cdnimg.melon.co.kr/svc/images/main/imgUrl20231114014430.jpg/melon/quality/80';
 
 const LargeAlbum = () => {
     //const [state, setState] = useState(LargeData.LargeData);
-    const Datas = LargeData.LargeData;
     const [startBtn, setStartBtn] = useState(false);
     let largeRef = useRef(0);
     let albumRef = useRef([]);
@@ -36,14 +38,22 @@ const LargeAlbum = () => {
     //         }
     //     }, 4000);
     // }
+    // img를 컴포넌트로 안하고 여기에 불러오면 어떨가 해서 실험
+    // const LargeImg = [
+    //     { image: picture1 },
+    //     { image: picture2 },
+    //     { image: picture3 },
+    //     { image: picture4 },
+    // ];
     // 3페어꺼 보고   clearinterval을빼먹은것을 알게됨
     useEffect(() => {
         if (startBtn === false) {
             const interval = setInterval(() => {
-                albumRef.current = LargeData.LargeData[largeRef.current];
+                albumRef.current = LargeData[largeRef.current];
                 console.log(largeRef.current);
                 largeRef.current++;
                 console.log(albumRef.current);
+                console.log(Image1);
                 //setState(state => albumRef.current);
                 if (largeRef.current > 3) {
                     largeRef.current = 0;
@@ -52,28 +62,49 @@ const LargeAlbum = () => {
             return () => clearInterval(interval);
         }
     }, [startBtn]);
-
-    const onClickBtn = () => {
+    //state이용해서 해야되는데 생각만하고 못함 +정리
+    const index0Btn = () => {
+        albumRef.current = 0;
+    };
+    const index1Btn = () => {
+        albumRef.current = 1;
+    };
+    const index2Btn = () => {
+        albumRef.current = 2;
+    };
+    const index3Btn = () => {
+        albumRef.current = 3;
+    };
+    const onStartBtn = () => {
         setStartBtn(prev => !prev);
     };
+
     return (
         <Styled.Wrapper>
             <Styled.EventList>
                 <Styled.PlayContain>
                     <Styled.MoveContain>
-                        <Styled.MoveBtn>o</Styled.MoveBtn>
-                        <Styled.MoveBtn>o</Styled.MoveBtn>
-                        <Styled.MoveBtn>o</Styled.MoveBtn>
-                        <Styled.MoveBtn>o</Styled.MoveBtn>
+                        <Styled.MoveBtn>
+                            <button onClick={index0Btn}>o</button>
+                        </Styled.MoveBtn>
+                        <Styled.MoveBtn>
+                            <button onClick={index1Btn}>o</button>
+                        </Styled.MoveBtn>
+                        <Styled.MoveBtn>
+                            <button onClick={index2Btn}>o</button>
+                        </Styled.MoveBtn>
+                        <Styled.MoveBtn>
+                            <button onClick={index3Btn}>o</button>
+                        </Styled.MoveBtn>
                     </Styled.MoveContain>
                     <Styled.PlayBtn>
-                        <button onClick={onClickBtn}>
+                        <button onClick={onStartBtn}>
                             {startBtn ? '>' : '='}
                         </button>
                     </Styled.PlayBtn>
                 </Styled.PlayContain>
 
-                <Styled.EventPart src={albumRef.current}></Styled.EventPart>
+                <Styled.EventPart></Styled.EventPart>
             </Styled.EventList>
         </Styled.Wrapper>
     );
@@ -94,7 +125,7 @@ const EventList = styled.div`
     position: relative;
 `;
 //사진 한장 처리
-const EventPart = styled.img`
+const EventPart = styled.a`
     width: 236px;
     height: 315px;
 `;
