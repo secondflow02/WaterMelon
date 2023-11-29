204aa9d2346ddd9d7ae9b9eff7f31dad02a7ffa0
keeprok <dldudfhr425@gmail.com>
2023-11-30 02:09:42 +0900

    feat(keeprok) 11/29 : LarfeAlbum이미지 파일 다시 복구및 js파일 useEffect 부분 이미지불러오기및 카운트까지 한곳에 넣은것을 분리 하면서 move버튼 기능추가

--- src/pages/main/components/LargeAlbum.js
+++ src/pages/main/components/LargeAlbum.js
@@ -14,70 +14,114 @@ const LargeAlbum = () => {
     //const [state, setState] = useState(LargeData.LargeData);
     const [Data, setData] = useState(LargeData);
     const [startBtn, setStartBtn] = useState(false);
-    let largeRef = useRef(0);
+    let countRef = useRef(0); //timer :
     let album = [];
+    album.current = LargeData[countRef.current];
+    const [lend, setlend] = useState(false);
+    //const [curIdx, setCurIdx] = useState(0)
+    // useEffect(()=>{
+    //     // curIdx 가 변경되면 4초 뒤에 변경된다.
+    //        setTimeout(() => {
+    //            setCurIdx((prev)=>(prev+1)%4)
+    //         }, 4000);
+    // },[curIdx, setCurIdx])
+    //4초마다 count 가 1씩 늘어남
     useEffect(() => {
         if (startBtn === false) {
             const interval = setInterval(() => {
-                album.current = LargeData[largeRef.current];
-
-                console.log(largeRef.current);
-                largeRef.current++;
-                console.log(LargeData[0].Image);
-                console.log(album.current.Image);
-                setData(Data => album.current);
-                //setState(state => albumRef.current);
-                if (largeRef.current > 3) {
-                    largeRef.current = 0;
+                console.log(countRef.current);
+                countRef.current++;
+                setlend(prev => !prev);
+                if (countRef.current > 3) {
+                    countRef.current = 0;
                 }
             }, 4000);
             return () => clearInterval(interval);
         }
+        //const [curIdx, setCurIdx] = useState(0)
     }, [startBtn]);
+    // count 1마다 사진이 변경됨?
+    useEffect(() => {
+        console.log(LargeData[0].Image);
+        console.log(album.current.Image);
+        setData(Data => album.current);
+        //setState(state => albumRef.current);
+    }, [album]);
+
     //로딩이되자마자 이벤트 실행( use effect)=>count 흐름
     // 버튼 클릭시 사진 변경(count마다 사진 변경그래야 movebtn 즉시 변화가능)
     // (new함수 생성=>currnt변수들 집어넣을 (사진들)
 
     //state이용해서 해야되는데 생각만하고 못함 +정리
     const index0Btn = () => {
-        largeRef.current = 0;
-        console.log(largeRef.current);
+        countRef.current = 0;
+        console.log(countRef.current);
+        setlend(prev => !prev);
     };
     const index1Btn = () => {
-        largeRef.current = 1;
-        console.log(largeRef.current);
+        countRef.current = 1;
+        console.log(countRef.current);
+        setlend(prev => !prev);
     };
     const index2Btn = () => {
-        largeRef.current = 2;
-        console.log(largeRef.current);
+        countRef.current = 2;
+        console.log(countRef.current);
+        setlend(prev => !prev);
     };
     const index3Btn = () => {
-        largeRef.current = 3;
-        console.log(largeRef.current);
+        countRef.current = 3;
+        console.log(countRef.current);
+        setlend(prev => !prev);
     };
     const onStartBtn = () => {
         setStartBtn(prev => !prev);
     };
-
+    //map 활용,뎁스 줄이기,
     return (
         <Styled.Wrapper>
             <Styled.EventList>
                 <Styled.PlayContain>
                     <Styled.MoveContain>
-                        <Styled.MoveBtn onClick={index0Btn}>o</Styled.MoveBtn>
-                        <Styled.MoveBtn onClick={index1Btn}>o</Styled.MoveBtn>
-                        <Styled.MoveBtn onClick={index2Btn}>o</Styled.MoveBtn>
-                        <Styled.MoveBtn onClick={index3Btn}>o</Styled.MoveBtn>
+                        <Styled.MoveBtn onClick={index0Btn}>
+                            {countRef.current === 0 ? (
+                                // eslint-disable-next-line react/jsx-pascal-case
+                                <Styled.isSelect>o</Styled.isSelect>
+                            ) : (
+                                'o'
+                            )}
+                        </Styled.MoveBtn>
+                        <Styled.MoveBtn onClick={index1Btn}>
+                            {countRef.current === 1 ? (
+                                // eslint-disable-next-line react/jsx-pascal-case
+                                <Styled.isSelect>o</Styled.isSelect>
+                            ) : (
+                                'o'
+                            )}
+                        </Styled.MoveBtn>
+                        <Styled.MoveBtn onClick={index2Btn}>
+                            {countRef.current === 2 ? (
+                                // eslint-disable-next-line react/jsx-pascal-case
+                                <Styled.isSelect>o</Styled.isSelect>
+                            ) : (
+                                'o'
+                            )}
+                        </Styled.MoveBtn>
+                        <Styled.MoveBtn onClick={index3Btn}>
+                            {countRef.current === 3 ? (
+                                // eslint-disable-next-line react/jsx-pascal-case
+                                <Styled.isSelect>o</Styled.isSelect>
+                            ) : (
+                                'o'
+                            )}
+                        </Styled.MoveBtn>
                     </Styled.MoveContain>
                     <Styled.PlayBtn>
-                        <button onClick={onStartBtn}>
-                            {startBtn ? '>' : '='}
-                        </button>
+                        <div onClick={onStartBtn}>{startBtn ? '▷' : 'II'}</div>
                     </Styled.PlayBtn>
                 </Styled.PlayContain>
 
                 <Styled.EventPart>
-                    <img src={Data.Image}></img>
+                    <img src={Data.Image} alt="Large"></img>
                 </Styled.EventPart>
             </Styled.EventList>
         </Styled.Wrapper>
@@ -102,6 +146,7 @@ const EventList = styled.div`
 const EventPart = styled.a`
     width: 236px;
     height: 315px;
+    top: 100px;
 `;
 
 //움직이는 버튼모음
@@ -111,6 +156,7 @@ const PlayContain = styled.div`
     right: 2px;
     display: flex;
     text-align: center;
+    display: flex;
 `;
 //시작 정지 버튼
 const PlayBtn = styled.a`
@@ -119,6 +165,7 @@ const PlayBtn = styled.a`
 //움직이는 버튼 모음
 const MoveContain = styled.div`
     margin-top: 2px;
+    display: flex;
 `;
 //동그라미 버튼
 const MoveBtn = styled.a`
@@ -130,6 +177,10 @@ const MoveBtn = styled.a`
         color: black;
     }
 `;
+const isSelect = styled.p`
+    color: black;
+    margin: 0;
+`;
 
 const Styled = {
     Wrapper,
@@ -139,6 +190,8 @@ const Styled = {
     PlayBtn,
     MoveContain,
     MoveBtn,
+    isSelect,
 };
+
 //사진 넘어가는 거 js 사진 연결되서 넘어가는 느낌
 //말
