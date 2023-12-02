c928a5e03500f7ebb2b03b9cff2683b3f492bb0e
bru37 <eevee4563@gmail.com>
2023-12-02 22:16:34 +0900

    FEAT(phill)12/02StyledComponents을사용해서새로만들어보았습니다.

--- src/pages/main/components/MelonCharts/Charts.js
+++ src/pages/main/components/MelonCharts/Charts.js
@@ -7,91 +7,37 @@ import chartData from '../../../../mock/chartData.json';
 // 2페어 차필우 허진욱
 const Charts = () => {
     return (
-        <>
-            <div class="chart" style={{ marginLeft: '24px' }}>
-                <div>
-                    <div
-                        style={{
-                            display: 'flex',
-                            alignItems: 'center',
-                            justifyContent: 'space-between',
-                        }}
-                    >
-                        <h2>
-                            <a href=""> 멜론 차트 </a>
-                        </h2>
-                        <div>2023.11.27 23:00 기준</div>
-                    </div>
-
-                    {/* 멜론차트 TOP100 POP 아티스트 */}
-                    <div
-                        style={{
-                            display: 'flex',
-                            justifyContent: 'space-between',
-                        }}
-                    >
-                        <button
-                            style={{ width: '95.856666px', height: '50px' }}
-                        >
-                            TOP100
-                        </button>
-                        <button style={{ width: '95.856666px' }}>POP</button>
-                        <button style={{ width: '95.856666px' }}>
-                            ☆ 아티스트
-                        </button>
-                    </div>
-                    {/* 멜론차트 1~10 */}
-                    <div>
-                        <ul
-                            style={{
-                                padding: '0px 0px 0px 0px',
-                                margin: '0px 0px 0px 0px',
-                            }}
-                        >
-                            {chartData.chartdata.map((el, index) => (
-                                <li
-                                    style={{
-                                        listStyleType: 'none',
-                                        border: '1px solid',
-                                    }}
-                                >
-                                    <span>
-                                        {chartData.chartdata[index].ranking}
-                                    </span>
-                                    <span>
-                                        {
-                                            chartData.chartdata[index]
-                                                .rankingChange
-                                        }
-                                    </span>
-                                    <span>
-                                        {chartData.chartdata[index].title}
-                                    </span>
-                                    <span>
-                                        {chartData.chartdata[index].singer}
-                                    </span>
-                                </li>
-                            ))}
-                        </ul>
-                    </div>
-                    {/* 더보기 */}
-                    <div
-                        style={{
-                            display: 'flex',
-                            alignItems: 'center',
-                            justifyContent: 'space-between',
-                            height: '50px',
-                            border: '1px solid',
-                        }}
-                    >
-                        <button style={{ margin: '0px 0px 0px 10px' }}>
-                            셔플듣기
-                        </button>
-                        <div style={{ margin: '0px 10px 0px 0px' }}>더보기</div>
-                    </div>
-                </div>
-            </div>
-        </>
+        <div>
+            <Styled.ChartTop>
+                <MelonCartBtn onClick={MelonCartClickBtn}>
+                    멜론차트
+                </MelonCartBtn>
+                <p>2030.12.02 18:00 기준</p>
+            </Styled.ChartTop>
+            <Styled.ChartTopBtn>
+                <button>TOP100</button>
+                <button>POP</button>
+                <button>아티스트</button>
+            </Styled.ChartTopBtn>
+            {chartData.chartdata.map((el, index) => (
+                <Styled.ChartList>
+                    <li>{chartData.chartdata[index].ranking}</li>
+                    <li>{chartData.chartdata[index].title}</li>
+                    <li>{chartData.chartdata[index].singer}</li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </Styled.ChartList>
+            ))}
+            <Styled.ChartLowBtn>
+                <button>셔플듣기</button>
+                <a>더보기</a>
+            </Styled.ChartLowBtn>
+        </div>
     );
 };
 export default Charts;
@@ -110,3 +56,41 @@ const VISITED = styled.div`
     color: #1a1a1a;
     text-decoration: none;
 `;
+// 제일 상단 멜론타트와 시간기준
+const ChartTop = styled.div`
+    display: flex;
+    justify-content: space-between;
+    position: relative;
+    bottom: 100px;
+`;
+//제일 상단 멜론차트버튼
+const MelonCartBtn = styled.button`
+    border: none;
+    background-color: white;
+    font-weight: 700;
+    font-size: 16px;
+`;
+//제일 상단 멜론차트 클릭버튼
+const MelonCartClickBtn = () => {};
+//TOP100,POP,아티스트 3개 버튼
+const ChartTopBtn = styled.div`
+    display: flex;
+`;
+//1~10까지 순위리스트
+const ChartList = styled.div`
+    list-style-type: none;
+    display: flex;
+    border: 1px solid;
+`;
+const ChartLowBtn = styled.div`
+    display: flex;
+    justify-content: space-between;
+`;
+
+const Styled = {
+    ChartTop,
+    ChartTopBtn,
+    ChartList,
+    MelonCartBtn,
+    ChartLowBtn,
+};
